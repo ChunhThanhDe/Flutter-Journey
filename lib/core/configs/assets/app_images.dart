622f12dd82e28994253e4d200e367a9def6042e6
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-11-22 16:58:11 +0700

    🎉 add assets

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -1,10 +1,12 @@
 class AppImages {
   static const String basePath = 'assets/images/';
 
-  static const String logoImg = '${basePath}me.jpg';
+  static const String logoImg = '${basePath}picture-logo.png';
+  static const String logo2Img = '${basePath}picture-logo2.png';
+  static const String meImg = '${basePath}picture-me.jpg';
 
-  static const String flameIcon = '${basePath}flame.png';
-
-  static const String searchIcon = '${basePath}search.png';
-  static const String toggleIcon = '${basePath}toggle_day.png';
+  static const String flameIcon = '${basePath}icon-flame.png';
+  static const String searchIcon = '${basePath}icon-search.png';
+  static const String toggleIcon = '${basePath}icon-toggle_day.png';
+  static const String dartIcon = '${basePath}icon-dart.png';
 }
